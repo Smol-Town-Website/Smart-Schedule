fc54d5ff3b8d03e7bc176e7c8d65c29d22636089
Infinity Chicken <117489481+InfinityChicken@users.noreply.github.com>
2022-11-05 18:13:43 +0000

    Update 11:13

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ const express = require('express')
 var app = express()
 const fs = require('fs')
 const users = JSON.parse(fs.readFileSync(__dirname + '/users.json'))
+const events = JSON.parse(fs.readFileSync(__dirname + '/events.json'))
 const friendships = JSON.parse(fs.readFileSync(__dirname + '/friendships.json'))
 
 app.get('/api/*', async (req, res) => {
+  parts = req.path.split('/').slice(3)
+  func = parts[0]
   if (req.path.startsWith('/api/users/')) {
-    parts = req.path.split('/').slice(3)
-    console.log(parts)
-    func = parts[0]
     if (func == 'get') {
       if (!(Number(parts[1]))) return
       founduser = users.find(u => u.id == Number(parts[1]))
-      if (!founduser) return
+      if (!founduser) return res.send({error: true})
       res.send(founduser)
     }
     if (func == 'addfriend') {
-      if (!(Number(parts[1]))) return
+      if (!(Number(parts[1]))) return res.send({error: true})
       id1 = Number(parts[1])
       if (!Number(req.query['target'])) return
       id2 = Number(req.query['target'])
@@ -26,7 +26,7 @@ app.get('/api/*', async (req, res) => {
       res.send({confirmation: true, newfriends: filtered})
     }
     if (func == 'friends') {
-      if (!(Number(parts[1]))) return
+      if (!(Number(parts[1]))) return res.send({error: true})
       filtered = friendships.filter(friendship => friendship.includes(Number(parts[1])))
       res.send({friends: filtered})
     }
@@ -37,6 +37,61 @@ app.get('/api/*', async (req, res) => {
         if (!founduser) return res.send({error: true})
         res.send(founduser)
       }
+    }
+  }
+  if (req.path.startsWith('/api/events/')) {
+    if (func == 'get') {
+      if (!Number(parts[1])) return res.send({error: true})
+      event = events.find(event => event.id == Number(parts[1]))
+      if (!event) return res.send({error: true})
+      res.send(event)
+    }
+    if (func == 'invite') {
+      if (!Number(parts[1])) return res.send({error: true})
+      if (!Number(req.query.sending_to)) return res.send({error: true})
+      if (!Number(req.query.event_id)) return res.send({error: true})
+      sender = Number(parts[1])
+      sending_to = Number(req.query.sending_to)
+      event_id = Number(req.query.event_id)
+      event = events.find(event => event.id == event_id)
+      event.users.push({id: sending_to, acceptance: false})
+      fs.writeFile(__dirname + '/events.json', JSON.stringify(events, null, 2), (err) => {if (err) throw err;})
+    }
+    if (func == 'new') {
+      data = JSON.parse(req.headers.data)
+      console.log(data)
+      userlist = [{
+        id: Number(parts[1]),
+        acceptance: true,
+        notes: "host"
+      }]
+      invited.forEach((invitedUserId) => {
+        userlist.push({
+          id: invitedUserId,
+          acceptance: false,
+          notes: "invited"
+        })
+      })
+      events.push({
+        users: userlist,
+        id: events.length-1,
+        info: {
+          name: data.name,
+          description: data.description,
+          location: data.location
+        },
+        time: {confirmed: false}
+      })
+      newEventId = events.length
+      res.send({
+        options: [data['possible-dates'][0]],
+        id: newEventId
+      })
+    }
+    if (func == 'confirm') {
+      eventId = parts[1]
+      start = req.query.start
+      finish = 
     }
   }
 })
