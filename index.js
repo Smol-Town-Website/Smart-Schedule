3d488f798cb3dd928ef520f60c26ad3db7cf19c2
SunGame7 <59129279+SunGame7@users.noreply.github.com>
2022-11-05 17:32:15 +0000

    minor updates

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ const friendships = JSON.parse(fs.readFileSync(__dirname + '/friendships.json'))
 
 app.get('/api/*', async (req, res) => {
   if (req.path.startsWith('/api/users/')) {
-    console.log(req.params)
-    path = req.path.slice(11)
-    console.log(path)
-    if (path.startsWith('get')) {
-      if (!(Number(path.slice(4)))) return
-      founduser = users.find(u => u.id == Number(path.slice(4)))
+    parts = req.path.split('/').slice(3)
+    console.log(parts)
+    func = parts[0]
+    if (func == 'get') {
+      if (!(Number(parts[1]))) return
+      founduser = users.find(u => u.id == Number(parts[1]))
       if (!founduser) return
       res.send(founduser)
     }
-    if (path.startsWith('addfriend')) {
-      if (!(Number(path.slice(10)))) return
-      id1 = Number(path.slice(10))
+    if (func == 'addfriend') {
+      if (!(Number(parts[1]))) return
+      id1 = Number(parts[1])
       if (!Number(req.query['target'])) return
       id2 = Number(req.query['target'])
       friendships.push([id1, id2])
@@ -25,13 +25,13 @@ app.get('/api/*', async (req, res) => {
       filtered = friendships.filter(friendship => friendship.includes(id1))
       res.send({confirmation: true, newfriends: filtered})
     }
-    if (path.startsWith('friends')) {
-      if (!(Number(path.slice(8)))) return
-      filtered = friendships.filter(friendship => friendship.includes(Number(path.slice(8))))
+    if (func == 'friends') {
+      if (!(Number(parts[1]))) return
+      filtered = friendships.filter(friendship => friendship.includes(Number(parts[1])))
       res.send({friends: filtered})
     }
-    if (path.startsWith('find/')) {
-      type = path.slice(5)
+    if (func == 'find') {
+      type = parts[1]
       if (type == 'email') {
         founduser = users.find(user => user.extra.email == req.query['input'])
         if (!founduser) return res.send({error: true})
